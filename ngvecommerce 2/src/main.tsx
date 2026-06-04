d30dff843effcb62a6fb6b3e78915f5b73ed0327
ngvdmr26 <vibecoder585@gmail.com>
2026-06-04 22:08:46 +0300

    Fix build: remove DownloadPrompt import from main.tsx

--- ngvecommerce 2/src/main.tsx
+++ ngvecommerce 2/src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { DownloadPrompt } from "./components/DownloadPrompt.tsx";
 import "./index.css";
-
-const AppEntry = () => {
-  if (window.location.pathname === "/download") {
-    return <DownloadPrompt />;
-  }
-  return <App />;
-};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppEntry />
+    <App />
   </StrictMode>,
 );
